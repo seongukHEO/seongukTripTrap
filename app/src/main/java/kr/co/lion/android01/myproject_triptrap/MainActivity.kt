b592de96e46da8b2d68c095bbb52bd7e674e4e5b
seongukHEO <heoseonguk3@gmail.com>
2024-02-12 15:51:56 +0900

    first

---
+++ app/src/main/java/kr/co/lion/android01/myproject_triptrap/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.lion.android01.myproject_triptrap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
